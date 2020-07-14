90fd9f1527ce921da3bf2cd520e20b2602628543
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-07-14 23:08:53 +0900

    remove gulp

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-
-gulp.task('lint', function() {
-  return gulp.src(['./main.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failOnError());
-});
